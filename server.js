4f823d413e16a0e4720d8d05a516507faa0d8d1f
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-12-21 18:40:03 -0500

    Create Views Directory and public folder

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
