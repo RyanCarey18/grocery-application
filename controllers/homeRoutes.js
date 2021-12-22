dd58f4585c04577292811160206c9e199d948996
Ryan Carey <ryaanski18@yahoo.com>
2021-12-21 19:05:37 -0500

    renamed the routes folder to controllers. Added a require(path) to server js. added the utils folder with helper and auth. added homeroutes file with included home routes written

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
