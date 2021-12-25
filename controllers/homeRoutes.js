7bd9185bc3de213919ac16f892915ef61b37f3ce
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-12-24 23:45:37 -0500

    Made all Web Pages Responsive and Render All Departments 's  name in The Home Page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,25 @@
 const router = require("express").Router();
+const { Aisle, Department, Product, User,
+} = require('../models');
 //const withAuth = require("../utils/auth");
 
+//Retrieve all the departments and render them to the home page
+
 router.get("/", async (req, res) => {
-  res.render("homepage");
+  try {
+    const AllDepartments = await Department.findAll();
+
+    const Departments = AllDepartments.map((dep) =>
+      dep.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      Departments,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/login", (req, res) => {
