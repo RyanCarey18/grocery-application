083da5fcc7d9e8a7f7531a1f4823e5fa47bc7181
Ryan Carey <ryaanski18@yahoo.com>
2022-01-06 22:10:05 -0500

    added prettier integration

--- public/assets/js/product.js
+++ public/assets/js/product.js
@@ -23,15 +23,8 @@ const addButtonHandler = async (event) => {
 const cartButtonHandler = async (event) => {
   event.preventDefault();
 
-  // Collect product id
-  const product_id = document.getElementById("add-btn").getAttribute("data-id");
-
-  //if there is a comment then send a request to server to create new comment
-  if (product_id) {
     const response = await fetch("/api/cart", {
-      method: "POST",
-      body: JSON.stringify({ product_id }),
-      headers: { "Content-Type": "application/json" },
+      method: "GET",
     });
 
     //if the request is okay refresh the post page
