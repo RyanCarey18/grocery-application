30fe6b2b1ac6a4b5aaa79004959487f00c45e64c
MERYEM-AD <adiri.meryem.mw@gmail.com>
2022-01-08 02:48:00 -0500

    secure products by department views through authentication, hashing password of users, adding sessions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const productRoutes = require("./product-routes");
 const departmentRoutes = require("./departmentRoutes");
 const aisleRoutes = require("./aisle-routes");
+const usersRoutes = require("./users-routes");
 const cartRoutes = require("./cartRoutes");
 
 router.use("/products", productRoutes);
 router.use("/departments", departmentRoutes);
 router.use("/aisles", aisleRoutes);
+router.use("/users", usersRoutes);
 router.use("/cart", cartRoutes);
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -47,11 +47,36 @@ User.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        // In this case, we are taking the user's email address, and making all letters lower case before adding it to the database.
+        newUserData.email = await newUserData.email.toLowerCase();
+        return newUserData;
+      },
+
+      beforeCreate: async (newUserData) => {
+        // In this case, we are taking the user's email address, and making all letters lower case before adding it to the database.
+        newUserData.password = await bcrypt.hash(newUserData.password,10);
+        return newUserData;
+      },
+      // Here, we use the beforeUpdate hook to make all of the characters lower case in an updated email address, before updating the database.
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.email = await updatedUserData.email.toLowerCase();
+        return updatedUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        if (updatedUserData.password) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        }
+        return updatedUserData;
+      },
+
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "user",
+    modelName: 'user',
   }
 );
 
